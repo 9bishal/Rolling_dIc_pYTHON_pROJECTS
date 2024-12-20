9f4910c72ae570def762095c23da958179e25625
Bishal kumar Shah <shahvishal.9090@gmail.com>
2024-12-20 11:28:48 +0530

    Update p1.py
    I modify. the project, on the basis so that the user can roll the multiple dice once.
    
    Even the user can know which dice rolled has got what value.

--- p1.py
+++ p1.py
@@ -2,9 +2,14 @@ import random
 while True:
     choice = input('Roll the dice?(y/n)').lower()
     if choice == 'y':
-        dice1 = random.randint(1,6)
-        # dice2 = random.randint(1,6)
-        print(f'({dice1})')
+        no_dice= int(input('How many dice you want to roll?'))
+        for i in range(no_dice):
+            dice1 = random.randint(1,6)
+            # dice2 = random.randint(1,6)
+
+            print(f'(for dice  {i+1}->{dice1})')
+        
+        
     elif choice =='n':
         print('Thanks for playing1')
         break
